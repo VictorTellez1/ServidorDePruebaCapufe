9aa96ebdc0a2e8d354091c296db30604df14a177
VictorTellez1 <victortellez691@gmail.com>
2023-04-19 14:29:56 -0500

    Cambiando token

--- controllers/capufe.js
+++ controllers/capufe.js
@@ -5,13 +5,10 @@ import { response,request } from "express";
 export const tokenGet=async(req,res=response)=>{
     const {token,caseta}=req.query
     console.log(token)
-    console.log(caseta)
     res.json({
         msg:"Enviado correctamente",
         token,
-        caseta
     })
-   
 }
 
 
@@ -20,7 +17,8 @@ export const tokenPost=async(req,res=response)=>{
     console.log(qr)
     res.json({
         msg:"Enviado correctamente",
-        qr
+        qr,
+        
     })
 }
 
